2fb1e796ff4dfa670fe9884c7334219967686f93
snwfdhmp <snwfdhmp@gmail.com>
2018-01-14 21:14:35 +0100

    update

--- util.go
+++ util.go
@@ -8,7 +8,7 @@ import (
 func RegexpWords(words ...string) *regexp.Regexp {
 	var arr []string
 	for _, w := range words {
-		arr = append(arr, "(.*(?i:"+w+").*)")
+		arr = append(arr, "\\b(?i:"+w+")")
 	}
 	regex := strings.Join(arr, "|")
 	return regexp.MustCompile(regex)
